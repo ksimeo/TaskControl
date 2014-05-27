331f3d7fa5433394e814a8d4f777d39d05b0f86f
Maksym.Fedorenko@gmail.com <Maksym.Fedorenko@gmail.com@67bbbdfd-cea2-cd1f-eecb-16ddecc761fd>
2014-05-27 17:23:23 +0000

    Init

---
+++ src/main/java/tests/TaskDaoTest.java
@@ -0,0 +1,47 @@
+package tests;
+
+import dao.TaskDao;
+import model.Task;
+
+import java.util.List;
+import java.util.Iterator;
+
+/**
+ * Created by Ksimeo on 27.05.2014.
+ */
+
+public class TaskDaoTest {
+
+    public static void main(String[] args)
+    {
+        TaskDao td = new TaskDao();
+        Task newTask = new Task( 1, "Погрузка", "Грузите ананасы бочками!", "Максимальный");
+
+        if (td.addTask(newTask))
+        {
+            System.out.println("SUCCESS");
+        }
+        else
+        {
+            System.out.println("Error");
+        }
+
+        List<Task> db = TaskDao.getAllTasks();
+
+        boolean res = td.addTask(newTask);
+        System.out.println("RES: " + res);
+        db = TaskDao.getAllTasks();
+        showTasks(db);
+    }
+
+    private static void showTasks(List<Task> all)
+    {
+        Iterator<Task> iter = all.iterator();
+        Task tmp;
+        while (iter.hasNext())
+        {
+            tmp = iter.next();
+            System.out.println(tmp);
+        }
+    }
+}
