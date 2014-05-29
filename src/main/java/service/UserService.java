26961b554d72c22f0215a97a94851192c1bff197
Maksym.Fedorenko@gmail.com <Maksym.Fedorenko@gmail.com@67bbbdfd-cea2-cd1f-eecb-16ddecc761fd>
2014-05-29 17:15:37 +0000

    added jsp page authorization and registration,  added servlets package

--- src/main/java/service/UserService.java
+++ src/main/java/service/UserService.java
@@ -1,7 +1,35 @@
 package service;
 
+import dao.UserDao;
+import model.User;
+
 /**
  * Created by Avega on 28.05.14.
  */
-public class UserService {
+public class UserService
+{
+    private UserDao usDao;
+
+    public UserService()
+    {
+        usDao = new UserDao();
+    }
+
+    public User saveUser(String name, String login, String password, int role)
+    {
+        User user = new User (name, login, password, role);
+
+        return usDao.saveUser(user) ? user : null;
+    }
+
+    public User searchUser(String name)
+    {
+        return usDao.searchUserByFullName(name);
+    }
+
+    public boolean checkLogin (String login, String password)
+    {
+        return null != usDao.getUserByLoginPassword(login, password);
+    }
+
 }
