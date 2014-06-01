f6d6ea35c4984011126fc213ff10846cf9f635cd
javavlad@gmail.com <javavlad@gmail.com@67bbbdfd-cea2-cd1f-eecb-16ddecc761fd>
2014-06-01 09:44:38 +0000

    Init

--- trunk/src/main/java/service/CurrentTaskService.java
+++ trunk/src/main/java/service/CurrentTaskService.java
@@ -8,42 +8,39 @@ import model.User;
 
 import java.util.Date;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * Created by VLAD on 27.05.2014.
  */
-public class CurrentTaskService
+public enum CurrentTaskService
 {
-    int nextId;
-    ICurrentTask ctd;
-    List<CurrentTask> ctasks;
+    INSTANCE;
+    private AtomicInteger nextId;
+    private ICurrentTask ctd;
 
-    public CurrentTaskService()
+    private CurrentTaskService()
     {
+
         ctd = new CurrentTaskDao();
-        ctasks = ctd.getAllCurrentTasks();
-        if(ctasks.size() > 0) {
-            nextId = getLastId() + 1;
-        }else {
-            nextId = 1;
-        }
+        int lastId = ctd.getLastId();
+        nextId = new AtomicInteger(lastId);
 
     }
 
     public boolean saveCurrentTask(int taskId, int creatorId, int recipientId, String state, String priority, Date createDate, Date startDate, Date endDate)
     {
 
-        CurrentTask ct = new CurrentTask(nextId, taskId, creatorId, recipientId, state, priority, createDate, startDate, endDate);
+        CurrentTask ct = new CurrentTask(nextId.get(), taskId, creatorId, recipientId, state, priority, createDate, startDate, endDate);
 
         boolean flag = ctd.saveCurrentTask(ct);
-        ctasks.add(ct);
-        nextId++;
+        nextId.incrementAndGet();
         return flag;
     }
 
     public List<CurrentTask> getAllCurrentTasks()
     {
-        return ctasks;
+        return ctd.getAllCurrentTasks();
     }
 
     public List<CurrentTask> getAllByUserId(User user)
@@ -62,11 +59,6 @@ public class CurrentTaskService
     public List<CurrentTask> getAllByTaskId(Task task)
     {
         return ctd.getAllByTaskId(task);
-    }
-
-    public int getLastId()
-    {
-        return ctd.getLastId();
     }
 
 }
