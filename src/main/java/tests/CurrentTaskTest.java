9f3cceb93f819149d2bb6809e98afd494c624abe
javavlad@gmail.com <javavlad@gmail.com@67bbbdfd-cea2-cd1f-eecb-16ddecc761fd>
2014-06-01 08:29:29 +0000

    Init

--- src/main/java/tests/CurrentTaskTest.java
+++ src/main/java/tests/CurrentTaskTest.java
@@ -20,32 +20,32 @@ public class CurrentTaskTest
         Date d = new Date();
         CurrentTaskService cts = new CurrentTaskService();
 
-        // Saving new current tasks --------------
-//        boolean res1 = cts.saveCurrentTask(1, 4, 9, CurrentTask.START, d, d, d);
+        //Saving new current tasks --------------
+//        boolean res1 = cts.saveCurrentTask(1, 4, 9, CurrentTask.START, "HAIGHT", d, d, d);
 //        System.out.println("Operation res1 is " + res1);
-//        boolean res2 = cts.saveCurrentTask(2, 2, 1, CurrentTask.IN_PROCESS, d, d, d);
+//        boolean res2 = cts.saveCurrentTask(2, 2, 1, CurrentTask.IN_PROCESS, "MIDDLE", d, d, d);
 //        System.out.println("Operation res2 is " + res2);
-//        boolean res3 = cts.saveCurrentTask(3, 2, 9, CurrentTask.FINISH, d, d, d);
+//        boolean res3 = cts.saveCurrentTask(3, 2, 9, CurrentTask.FINISH, "LOW", d, d, d);
 //        System.out.println("Operation res2 is " + res3);
 //        System.out.println("Saving is ready");
 
 
         // getting all --------------
-        User recipient = new User("Vasya Pupkin","vasya123", "qwert", 1);
-        recipient.setUserId(9);
-        User creator = new User("Kolya, Pupkin","vasya123", "qwert", 1);
-        creator.setUserId(2);
-        Task task1 = new Task(1,"Title","Some rules","LOW");
-
-        // getting all current tasks --------------
-        print(cts.getAllCurrentTasks());
-        // getting all current tasks by recipient id --------------
-        print(cts.getAllByUserId(recipient));
-        // getting all current tasks by creator id --------------
-        print(cts.getAllByCreatorId(creator));
-        // getting all current tasks by task id --------------
-        print(cts.getAllByTaskId(task1));
-
+//        User recipient = new User("Vasya Pupkin","vasya123", "qwert", 1);
+//        recipient.setUserId(9);
+//        User creator = new User("Kolya, Pupkin","vasya123", "qwert", 1);
+//        creator.setUserId(2);
+//        Task task1 = new Task(1,"Title","Some rules");
+//
+//        // getting all current tasks --------------
+//        print(cts.getAllCurrentTasks());
+//        // getting all current tasks by recipient id --------------
+//        print(cts.getAllByUserId(recipient));
+//        // getting all current tasks by creator id --------------
+//        print(cts.getAllByCreatorId(creator));
+//        // getting all current tasks by task id --------------
+//        print(cts.getAllByTaskId(task1));
+        System.out.println();
 
     }
 
