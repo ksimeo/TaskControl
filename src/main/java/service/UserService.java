150575dc016c715117c006069a9ce2b9c385e6f7
Maksym.Fedorenko@gmail.com <Maksym.Fedorenko@gmail.com@67bbbdfd-cea2-cd1f-eecb-16ddecc761fd>
2014-06-01 17:33:27 +0000

    added interface

--- src/main/java/service/UserService.java
+++ src/main/java/service/UserService.java
@@ -6,7 +6,7 @@ import model.User;
 /**
  * Created by Avega on 28.05.14.
  */
-public class UserService
+public class UserService implements IUserService
 {
     private UserDao usDao;
 
@@ -14,19 +14,19 @@ public class UserService
     {
         usDao = new UserDao();
     }
-
+    @Override
     public User saveUser(String name, String login, String password, int role)
     {
         User user = new User (name, login, password, role);
 
         return usDao.saveUser(user) ? user : null;
     }
-
+    @Override
     public User searchUser(String name)
     {
         return usDao.searchUserByFullName(name);
     }
-
+    @Override
     public boolean checkLogin (String login, String password)
     {
         return null != usDao.getUserByLoginPassword(login, password);
